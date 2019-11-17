84f7a95edb160ab9fb1cb4d387bfa9d89ac8b6bc
KennedyDias <kennedysouzadias@gmail.com>
2019-11-17 20:23:20 -0300

    Implement ViewModel from Gallery screen

--- app/src/main/java/com/kennedydias/dogspictures/di/KoinAppModules.kt
+++ app/src/main/java/com/kennedydias/dogspictures/di/KoinAppModules.kt
@@ -2,11 +2,17 @@ package com.kennedydias.dogspictures.di
 
 import com.kennedydias.dogspictures.ui.breeds.BreedsViewModel
 import com.kennedydias.dogspictures.ui.gallery.GalleryViewModel
+import com.kennedydias.dogspictures.utils.ResourcesUtils
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val viewModelModules: Module = module {
     viewModel { BreedsViewModel(get()) }
-    viewModel { GalleryViewModel() }
+    viewModel { GalleryViewModel(get(), get()) }
+}
+
+val utilsModules: Module = module {
+    factory { ResourcesUtils(androidContext()) }
 }

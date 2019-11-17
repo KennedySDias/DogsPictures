d38c05d057c2dc679359951480a5dc71836e04d0
KennedyDias <kennedysouzadias@gmail.com>
2019-11-17 20:38:51 -0300

    Make BreedData model as Parcelable

--- domain/src/main/java/com/kennedydias/domain/model/BreedData.kt
+++ domain/src/main/java/com/kennedydias/domain/model/BreedData.kt
@@ -1,7 +1,11 @@
 package com.kennedydias.domain.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class BreedData(
     val id: Int,
     val name: String,
     val key: String
-)
+) : Parcelable
